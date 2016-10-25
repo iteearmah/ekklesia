8b1bbd04d012f105367961cdd19021b15c4b0a6d
Samuel Armah <iteearmah@gmail.com>
2016-10-25 17:20:47 +0000

    QR scanner corrected

--- pages/events_details.js
+++ pages/events_details.js
@@ -9,9 +9,41 @@ exports.createEventsDetailsPage = function (event_item, page_margin) {
         id: 'scanQRButton',
         text: 'Scan QR',
         background: '#114A59',
-        layout:{ top: ['50%',0],left:['50%',10]},
+        layoutData: {
+            centerX: 0,
+            centerY: 0,
+            width: 100,
+            height: 100
+        },
         textColor: 'white'
     }).appendTo(page);
+    scanQRButton.on('select', function () {
 
+        var callback = function (err, contents) {
+            if (err) {
+                navigator.notification.alert(err._message);
+            } else {
+                var user = JSON.parse(localStorage.getItem('user'));
+                clockIn(event_item.id, 1, user.token);
+                
+            }
+
+        };
+
+        QRScanner.scan(callback);
+    });
     return page;
+}
+var clockIn = function (event_id, member_id, token) {
+    utils.getJSON('event/clock_in?id=' + event_id + '&member_id=' + member_id + '&token=' + token).then(function (json) {
+        console.log(JSON.stringify(json));
+        navigator.notification.alert(
+            json.data.message, // message
+            function () {
+                 QRScanner.destroy(function (status) {});
+            }, // callback
+            "QR", // title
+            "Done" // buttonName
+        );
+    });
 }
